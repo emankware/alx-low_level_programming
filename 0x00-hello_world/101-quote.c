17063637925cd0d47b7ebf3c00c4230a3cfaeb13
emankware <lucymankware@gmail.com>
2023-03-16 19:57:19 -0700

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - not using normal terms
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
