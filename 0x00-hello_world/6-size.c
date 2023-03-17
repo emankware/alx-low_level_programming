badff724eabd549be4dd3ebe78d33c1cdb4dcf76
emankware <lucymankware@gmail.com>
2023-03-16 19:11:17 -0700

    bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - counts the size in bytes
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;/*represents char*/
+	int i;/*represents interger*/
+	int f;/*represents float*/
+	int l;/*represents long interger*/
+
+	printf("the size of char,\n");
+	scanf("%c", &c);
+	printf("the size of float,\n");
+	scanf("%f", &f);
+	printf("the size of interger,\n");
+	scanf("%d", &d);
+	printf("the size of long interger,\n");
+	scanf("%ld", &l);
+	return (0);
+}
+
+
