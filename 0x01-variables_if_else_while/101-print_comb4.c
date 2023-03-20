eebb2030113ce356345d7bfcb34571cfc383fc61
emankware <lucymankware@mail.com>
2023-03-19 17:44:34 -0700

    print combo 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,20 +11,20 @@ int main(void)
 	int tens = '0';
 	int hundreds = '0';
 
-	for (hundreds = '0'; hundreds <= '0'; hundreds++)
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		for (tens = '0'; tens <= '0'; tens++)
+		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for (ones = '0'; ones <= '0'; ones++)
+			for (ones = '0'; ones <= '9'; ones++)
 			{
 				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens))) /*eliminates repititon*/
+						(tens > ones) || (hundreds > tens))) /*eliminates repititon*/
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
 					if (!(ones == '9' && hundreds == '7' &&
-								tens == '8')) /*adds commas and spaces*/
+							tens == '8')) /*adds commas and spaces*/
 					{
 						putchar(',');
 						putchar(' ');
