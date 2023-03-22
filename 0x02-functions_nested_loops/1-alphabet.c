8fcc3f663023d1d02263b21f1825cea2fd37722a
emankware <lucymankware@mail.com>
2023-03-22 09:18:16 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *printalphabet - writes alphabet in lowercase
+  *
+  *Return: void
+  */
+void printalphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+}
