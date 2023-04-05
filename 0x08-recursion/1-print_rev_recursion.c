05eb3ec4d827057426a2162667a7d0667b10e651
emankware <lucymankware@mail.com>
2023-04-05 15:46:06 -0700

    recursion_reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s[0] != '\0')
 	{
-		_putchar(*s);
+		_putchar(s[0]);
 		_print_rev_recursion(s + 1);
 	}
-	else
-	{
-		_putchar('\n');
-	}
-
+		
 }
