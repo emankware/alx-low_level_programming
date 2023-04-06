8dd238277882591e47476439f060def45065d60d
emankware <lucymankware@mail.com>
2023-04-06 13:53:20 -0700

    prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,39 @@
 #include "main.h"
 
+int _divisible(int t, int j);
+
 /**
-  *
-  *
-  *
-  *
+  * _divisible - entry point
+  * @t: input
+  * @j: input
+  * Return:  1 if prime number otherwise 0
+ */
+
+int _divisible(int t, int j)
+{
+	if (j == 1 || j == t)
+		return (0);
+	if (t % j == 0)
+		return (1);
+
+	return (_divisible(t, j - 1));
+
+}
+
+
+/**
+  * is_prime_number - function that returns a prime number
+  * @n: input
+  * Return: 1 if prime number otherwise 0
   */
 
 int is_prime_number(int n)
 {
-	ggh
+	if (n <= 1)
+	return (0);
+	if (n == 2)
+	return (1);
 
+	return (!_divisible(n, n - 1));
 
 }
