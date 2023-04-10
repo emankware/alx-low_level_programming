0a1d31d29d99203bbccc0632f2d75db0f62e2ae7
emankware <lucymankware@mail.com>
2023-04-10 16:41:01 -0700

    all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int c;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+		printf("%d\n", c);
+	}
+	return (0);
+
+}
