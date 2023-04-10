20fdbdbf2dcdfacfe77fbc16530eddb1cfc45a31
emankware <lucymankware@mail.com>
2023-04-10 15:20:22 -0700

    lb static

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -13,11 +13,14 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+	{
 	if (s[i] == c)
 		return (s + i);
+	
 	else
 		return (NULL);
 
+	}
+	return (0);
+
 }
-
-
