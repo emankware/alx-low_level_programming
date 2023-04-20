401fdb289104e654acf52f4d2b89f6c167a026ba
emankware <lucymankware@mail.com>
2023-04-20 15:55:39 -0700

    search interger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of the first element
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to function
+ * Return: 0 on success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]))
+			return (j);
+	}
+	return (-1);
+}
