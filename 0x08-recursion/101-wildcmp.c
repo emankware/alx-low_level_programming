d65db5fed58bb9b228860e13ed19c5667125a3fb
emankware <lucymankware@gmail.com>
2023-05-02 15:21:05 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * wildcmp - function that compares strings
+ * @s1: base address for string
+ * @s2: base address for string
+ * Return: always (0)
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
